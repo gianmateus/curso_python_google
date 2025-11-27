6228cbe3a1d14873f8d231bd15c5fc6b771494ce
Mateus <gianmateus22@icloud.com>
2025-11-27 12:10:42 +0100

    feat: add LetterCompiler function using regex and its corresponding unit tests

---
+++ curso_python_google/testes_em_python/unit_tests_and_edge_cases_v3.py
@@ -0,0 +1,28 @@
+import re
+import unittest
+my_txt = "An investment in knowledge pays the best interest."
+
+def LetterCompiler(txt):
+    result = re.findall(r"([a-c]).", txt)
+    return result
+
+print(LetterCompiler(my_txt))
+
+class TestCompiler(unittest.TestCase):
+
+    def test_basic(self):
+        testcase = "The best preparation for tomorrow is doing your best today."
+        expected = ['b', 'a', 'a', 'b', 'a']
+        self.assertEqual(LetterCompiler(testcase), expected)
+
+
+
+class TestCompiler2(unittest.TestCase):
+
+    def test_two(self):
+        testcase = "A b c d e f g h i j k l m n o p q r s t u v w x y z"
+        expected = ['b', 'c']
+        self.assertEqual(LetterCompiler(testcase), expected)
+
+
+unittest.main(argv = ['first-arg-is-ignored'], exit = False)
